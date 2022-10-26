6447c7bf427049ae8a3493251552c7e82eab1251
Carlos Eduardo <33350010+CarlosEduardoFSouza@users.noreply.github.com>
2022-10-26 23:22:13 +0000

    input

---
+++ aula1-2022-10-19.py
@@ -0,0 +1,16 @@
+# Meu primeiro projeto python!!
+#
+# print()  = comando de saida
+
+print('Hello World')
+
+# Guardar uma String
+nome = 'Carlos Souza'
+# Guardar um numero
+idade = 20
+
+#Exibir o nome
+print(f'Meu nome é {nome}, tenho {idade} anos')
+'''
+  comentario em bloco
+'''
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# Meu primeiro projeto python!!
-#
-# print()  = comando de saida
+# Comando input(): quero permitir que  o usuario digite algo...
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+# Comando de saida...exibir na tela
+print(f"Boa noite! Seu nome é {nome} e, você tem {idade} anos")
 
-print('Hello World')
-
-# Guardar uma String
-nome = 'Carlos Souza'
-# Guardar um numero
-idade = 20
-
-#Exibir o nome
-print(f'Meu nome é {nome}, tenho {idade} anos')
-'''
-  comentario em bloco
-'''
+dobro = idade * 2
+print(f"O dobro da sua idade é {dobro}")
